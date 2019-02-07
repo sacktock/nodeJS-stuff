70d351cd0b716234a49df212b82c3f5539c66082
sacktock <alex.w.goodall@gmail.com>
2019-02-07 15:08:35 +0000

    commit with nodejs_server.js and week 14 prog exercise

---
+++ example week 14/index.js
@@ -0,0 +1,5 @@
+var adjectives = ['sweaty','cool','smelly','huge'];
+var nouns = ['pizza','meme','person','video game'];
+var verbs = ['ran','left','hated','ate'];
+var adverbs = ['properly','quickly','furiously','stupidly'];
+//could use data muse API to get words
